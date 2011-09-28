97a1a0ca270e753b1b025fb157c6841a76df4cc5
Cary Lee <carylee@gmail.com>
2011-09-28 13:21:38 -0500

    Added initial rails app

---
+++ metrovote/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metrovote::Application.config.secret_token = '5c702f80a410ef63b5969f5da02de7dfee6f937a8c41023062c3f2da45f5bd03201974f0c3f5aa5a906b811d758ac6f50437ed98821c8c4676b0cbc6791af0b4'
---
+++ metrovote/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Metrovote::Application.config.session_store :cookie_store, :key => '_metrovote_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Metrovote::Application.config.session_store :active_record_store
