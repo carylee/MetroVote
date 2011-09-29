3f6e3131eea37f8dd0e7403e18aa3812456ee4c0
Cary Lee <carylee@gmail.com>
2011-09-28 19:41:22 -0500

    Added seed data

--- metrovote/db/seeds.rb
+++ metrovote/db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Candidate.delete_all
+
+Candidate.create(:name => "Bobby Forch",
+                 :position => "Council Seat 1",
+                 :website => "http://bobbyforch.com",
+                 :twitter => "BobbyForch",
+                 :facebook => "http://www.facebook.com/pages/Bobby-Forch/80010933301",
+                 :email => "forchforcouncil@gmail.com",
+                 :photo => "http://washingtonbus.org/blog/blog-images/forch.jpg"
+                )
+
+Candidate.create(:name => "Jean Godden",
+                 :position => "Council Seat 1",
+                 :website => "http://www.jeangodden.com",
+                 :twitter => "SeattleScribe",
+                 :facebook => "http://www.facebook.com/reelectjeangodden",
+                 :email => "info@jeangodden.com",
+                 :photo => ""
+                )
+
+Candidate.create(:name => "Bruce Harrell",
+                 :position => "Council Seat 3",
+                 :website => "http://www.electbruceharrell.com",
+                 :twitter => "bruceharrell",
+                 :facebook => "http://www.facebook.com/bruceharrell",
+                 :email => "bruceharrell2011@gmail.com",
+                 :photo => ""
+                )
+
+Candidate.create(:name => "Brad Meacham",
+                 :position => "Council Seat 3",
+                 :website => "http://www.electbradmeacham.com",
+                 :twitter => "bradmeacham",
+                 :facebook => "http://www.facebook.com/electbradmeacham",
+                 :email => "forchforcouncil@gmail.com",
+                 :photo => ""
+                )
