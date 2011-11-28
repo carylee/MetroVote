95e7245f1c36808e9289f3e5822f9c342eece96c
Cary Lee <carylee@gmail.com>
2011-11-27 23:17:58 -0600

    Updated news source list

--- metrovote/app/models/election.rb
+++ metrovote/app/models/election.rb
@@ -1,7 +1,17 @@
 class Election < ActiveRecord::Base
   has_many :positions
 
+  def candidate_ids
+    ids = []
+    self.positions.each do |position|
+      position.candidates.each do |candidate|
+        ids.push(candidate.id)
+      end
+    end
+    return ids
+  end
+
   def article_sources
-    Article.find_by_sql("select source,host from articles group by source")
+    Article.find_by_sql ["select source,host from articles where candidate_id IN (?) group by source", self.candidate_ids.join(', ')]
   end
 end
